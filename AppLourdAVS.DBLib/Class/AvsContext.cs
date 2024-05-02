e052d4cd9013b5e33f0b6fafcbbf1af9a6400a1c
Mateo <mateoggn04@gmail.com>
2024-05-02 14:53:02 +0200

    transfert de la base de donnée en CSharp

---
+++ AppLourdAVS.DBLib/Class/AvsContext.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppLourdAVS.DBLib.Class;
+
+public partial class AvsContext : DbContext
+{
+    public AvsContext()
+    {
+    }
+
+    public AvsContext(DbContextOptions<AvsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Ability> Abilities { get; set; }
+
+    public virtual DbSet<AssignedRole> AssignedRoles { get; set; }
+
+    public virtual DbSet<Consultation> Consultations { get; set; }
+
+    public virtual DbSet<Demande> Demandes { get; set; }
+
+    public virtual DbSet<FailedJob> FailedJobs { get; set; }
+
+    public virtual DbSet<Migration> Migrations { get; set; }
+
+    public virtual DbSet<PasswordResetToken> PasswordResetTokens { get; set; }
+
+    public virtual DbSet<Permission> Permissions { get; set; }
+
+    public virtual DbSet<PersonalAccessToken> PersonalAccessTokens { get; set; }
+
+    public virtual DbSet<Praticien> Praticiens { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<Type> Types { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("Server=localhost;Database=avs;user=root;password=;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ability>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("abilities");
+
+            entity.HasIndex(e => e.Scope, "abilities_scope_index");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.EntityId).HasColumnName("entity_id");
+            entity.Property(e => e.EntityType)
+                .HasMaxLength(255)
+                .HasColumnName("entity_type");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.OnlyOwned).HasColumnName("only_owned");
+            entity.Property(e => e.Options)
+                .HasColumnType("json")
+                .HasColumnName("options");
+            entity.Property(e => e.Scope).HasColumnName("scope");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .HasColumnName("title");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+        });
+
+        modelBuilder.Entity<AssignedRole>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("assigned_roles");
+
+            entity.HasIndex(e => new { e.EntityId, e.EntityType, e.Scope }, "assigned_roles_entity_index");
+
+            entity.HasIndex(e => e.RoleId, "assigned_roles_role_id_index");
+
+            entity.HasIndex(e => e.Scope, "assigned_roles_scope_index");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.EntityId).HasColumnName("entity_id");
+            entity.Property(e => e.EntityType).HasColumnName("entity_type");
+            entity.Property(e => e.RestrictedToId).HasColumnName("restricted_to_id");
+            entity.Property(e => e.RestrictedToType)
+                .HasMaxLength(255)
+                .HasColumnName("restricted_to_type");
+            entity.Property(e => e.RoleId).HasColumnName("role_id");
+            entity.Property(e => e.Scope).HasColumnName("scope");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.AssignedRoles)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("assigned_roles_role_id_foreign");
+        });
+
+        modelBuilder.Entity<Consultation>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("consultations");
+
+            entity.HasIndex(e => e.PraticienId, "consultations_praticien_id_foreign");
+
+            entity.HasIndex(e => e.TypeId, "consultations_type_id_foreign");
+
+            entity.HasIndex(e => e.UserId, "consultations_user_id_foreign");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Date)
+                .HasColumnType("date")
+                .HasColumnName("date");
+            entity.Property(e => e.Deadline)
+                .HasColumnType("date")
+                .HasColumnName("deadline");
+            entity.Property(e => e.Delay).HasColumnName("delay");
+            entity.Property(e => e.PraticienId).HasColumnName("praticien_id");
+            entity.Property(e => e.TypeId).HasColumnName("type_id");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.Praticien).WithMany(p => p.Consultations)
+                .HasForeignKey(d => d.PraticienId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("consultations_praticien_id_foreign");
+
+            entity.HasOne(d => d.Type).WithMany(p => p.Consultations)
+                .HasForeignKey(d => d.TypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("consultations_type_id_foreign");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Consultations)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("consultations_user_id_foreign");
+        });
+
+        modelBuilder.Entity<Demande>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("demandes");
+
+            entity.HasIndex(e => e.PraticienId, "demandes_praticien_id_foreign");
+
+            entity.HasIndex(e => e.TypeId, "demandes_type_id_foreign");
+
+            entity.HasIndex(e => e.UserId, "demandes_user_id_foreign");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Date)
+                .HasColumnType("date")
+                .HasColumnName("date");
+            entity.Property(e => e.Deadline)
+                .HasColumnType("date")
+                .HasColumnName("deadline");
+            entity.Property(e => e.Delay).HasColumnName("delay");
+            entity.Property(e => e.PraticienId).HasColumnName("praticien_id");
+            entity.Property(e => e.TypeId).HasColumnName("type_id");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.Praticien).WithMany(p => p.Demandes)
+                .HasForeignKey(d => d.PraticienId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("demandes_praticien_id_foreign");
+
+            entity.HasOne(d => d.Type).WithMany(p => p.Demandes)
+                .HasForeignKey(d => d.TypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("demandes_type_id_foreign");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Demandes)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("demandes_user_id_foreign");
+        });
+
+        modelBuilder.Entity<FailedJob>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("failed_jobs");
+
+            entity.HasIndex(e => e.Uuid, "failed_jobs_uuid_unique").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Connection)
+                .HasColumnType("text")
+                .HasColumnName("connection");
+            entity.Property(e => e.Exception).HasColumnName("exception");
+            entity.Property(e => e.FailedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP")
+                .HasColumnType("timestamp")
+                .HasColumnName("failed_at");
+            entity.Property(e => e.Payload).HasColumnName("payload");
+            entity.Property(e => e.Queue)
+                .HasColumnType("text")
+                .HasColumnName("queue");
+            entity.Property(e => e.Uuid).HasColumnName("uuid");
+        });
+
+        modelBuilder.Entity<Migration>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("migrations");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Batch).HasColumnName("batch");
+            entity.Property(e => e.Migration1)
+                .HasMaxLength(255)
+                .HasColumnName("migration");
+        });
+
+        modelBuilder.Entity<PasswordResetToken>(entity =>
+        {
+            entity.HasKey(e => e.Email).HasName("PRIMARY");
+
+            entity.ToTable("password_reset_tokens");
+
+            entity.Property(e => e.Email).HasColumnName("email");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Token)
+                .HasMaxLength(255)
+                .HasColumnName("token");
+        });
+
+        modelBuilder.Entity<Permission>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("permissions");
+
+            entity.HasIndex(e => e.AbilityId, "permissions_ability_id_index");
+
+            entity.HasIndex(e => new { e.EntityId, e.EntityType, e.Scope }, "permissions_entity_index");
+
+            entity.HasIndex(e => e.Scope, "permissions_scope_index");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.AbilityId).HasColumnName("ability_id");
+            entity.Property(e => e.EntityId).HasColumnName("entity_id");
+            entity.Property(e => e.EntityType).HasColumnName("entity_type");
+            entity.Property(e => e.Forbidden).HasColumnName("forbidden");
+            entity.Property(e => e.Scope).HasColumnName("scope");
+
+            entity.HasOne(d => d.Ability).WithMany(p => p.Permissions)
+                .HasForeignKey(d => d.AbilityId)
+                .HasConstraintName("permissions_ability_id_foreign");
+        });
+
+        modelBuilder.Entity<PersonalAccessToken>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("personal_access_tokens");
+
+            entity.HasIndex(e => e.Token, "personal_access_tokens_token_unique").IsUnique();
+
+            entity.HasIndex(e => new { e.TokenableType, e.TokenableId }, "personal_access_tokens_tokenable_type_tokenable_id_index");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Abilities)
+                .HasColumnType("text")
+                .HasColumnName("abilities");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.ExpiresAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("expires_at");
+            entity.Property(e => e.LastUsedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("last_used_at");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.Token)
+                .HasMaxLength(64)
+                .HasColumnName("token");
+            entity.Property(e => e.TokenableId).HasColumnName("tokenable_id");
+            entity.Property(e => e.TokenableType).HasColumnName("tokenable_type");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+        });
+
+        modelBuilder.Entity<Praticien>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("praticiens");
+
+            entity.HasIndex(e => e.TypeId, "praticiens_type_id_foreign");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Job)
+                .HasMaxLength(255)
+                .HasColumnName("job");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.TypeId).HasColumnName("type_id");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+
+            entity.HasOne(d => d.Type).WithMany(p => p.Praticiens)
+                .HasForeignKey(d => d.TypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("praticiens_type_id_foreign");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("roles");
+
+            entity.HasIndex(e => new { e.Name, e.Scope }, "roles_name_unique").IsUnique();
+
+            entity.HasIndex(e => e.Scope, "roles_scope_index");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Scope).HasColumnName("scope");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .HasColumnName("title");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+        });
+
+        modelBuilder.Entity<Type>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("types");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Duration).HasColumnName("duration");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("users");
+
+            entity.HasIndex(e => e.Email, "users_email_unique").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Birthday)
+                .HasColumnType("date")
+                .HasColumnName("birthday");
+            entity.Property(e => e.Client)
+                .IsRequired()
+                .HasDefaultValueSql("'1'")
+                .HasColumnName("client");
+            entity.Property(e => e.CreatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("created_at");
+            entity.Property(e => e.Email).HasColumnName("email");
+            entity.Property(e => e.EmailVerifiedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("email_verified_at");
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(255)
+                .HasColumnName("firstname");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(255)
+                .HasColumnName("gender");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .HasColumnName("password");
+            entity.Property(e => e.RememberToken)
+                .HasMaxLength(100)
+                .HasColumnName("remember_token");
+            entity.Property(e => e.UpdatedAt)
+                .HasColumnType("timestamp")
+                .HasColumnName("updated_at");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
